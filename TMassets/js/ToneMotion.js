b55c607a5b23b99837b7431d895ecacc8f54b710
Ryan Carter <ryan@ryancarter.org>
2019-07-23 17:29:04 -0400

    Revert "need to add motino event listener again? probably was denied at first."
    This reverts commit 1240229369967a51c8aa74e2b03b97a8a5c98a4d.

--- TMassets/js/ToneMotion.js
+++ TMassets/js/ToneMotion.js
@@ -75,7 +75,6 @@ var testForMotion = (function() {
         // I hope a dialog window will appear asking user permission for motion data access. Temporary workaround below. Not elegant.
         if (window.confirm("No motion detected. Check permissions. Select 'Ok' to try again or 'Cancel' to launch desktop simulation.")) {
           // user has opted to try again
-          window.addEventListener("devicemotion", handleMotionEvent, true);
           counter = 1;
           testForMotion();
         } else {
